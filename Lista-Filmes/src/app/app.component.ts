f0fda0f1dcd652f8197f21f478ae2c7e5af10cd8
Bruno.Lafayette <brunolafayette@rocketmail.com>
2023-11-24 21:06:15 -0300

    atualizado lista de filmes

--- Lista-Filmes/src/app/app.component.ts
+++ Lista-Filmes/src/app/app.component.ts
@@ -10,19 +10,33 @@ import { FormsModule } from '@angular/forms';
 })
 export class AppComponent {
   filmes = [
-    { nome: 'Filme 1', categoria: 'Ação' },
-    { nome: 'Filme 2', categoria: 'Comédia' },
-    { nome: 'Filme 3', categoria: 'Drama' },
-    { nome: 'Filme 1', categoria: 'Ação' },
-    { nome: 'Filme 2', categoria: 'Comédia' },
-    { nome: 'Filme 3', categoria: 'Drama' },
-    { nome: 'Filme 1', categoria: 'Ação' },
-    { nome: 'Filme 2', categoria: 'Comédia' },
-    { nome: 'Filme 3', categoria: 'Drama' },
-    { nome: 'Filme 1', categoria: 'Ação' },
-    { nome: 'Filme 2', categoria: 'Comédia' },
-    { nome: 'Filme 3', categoria: 'Drama' },
+    { nome: 'Rambo: First Blood', categoria: 'Ação' },
+    { nome: 'Duro de Matar', categoria: 'Ação' },
+    { nome: 'Mad Max: Estrada da Fúria', categoria: 'Ação' },
+    { nome: 'Matrix', categoria: 'Ação' },
+    { nome: 'O Ultimato Bourne', categoria: 'Ação' },
+    { nome: 'Se Beber, Não Case!', categoria: 'Comédia' },
+    { nome: 'Superbad', categoria: 'Comédia' },
+    { nome: 'Quero Ser Grande', categoria: 'Comédia' },
+    { nome: 'Borat', categoria: 'Comédia' },
+    { nome: 'As Branquelas', categoria: 'Comédia' },
+    { nome: 'O Poderoso Chefão', categoria: 'Drama' },
+    { nome: 'Clube da Luta', categoria: 'Drama' },
+    { nome: 'À Espera de Um Milagre', categoria: 'Drama' },
+    { nome: 'A Lista de Schindler', categoria: 'Drama' },
+    { nome: 'O Curioso Caso de Benjamin Button', categoria: 'Drama' },  
+    { nome: 'O Exorcista', categoria: 'Terror' },
+    { nome: 'O Iluminado', categoria: 'Terror' },
+    { nome: 'Invocação do Mal', categoria: 'Terror' },
+    { nome: 'O Chamado', categoria: 'Terror' },
+    { nome: 'Atividade Paranormal', categoria: 'Terror' },  
+    { nome: 'Psicose', categoria: 'Suspense' },
+    { nome: 'O Silêncio dos Inocentes', categoria: 'Suspense' },
+    { nome: 'Seven: Os Sete Crimes Capitais', categoria: 'Suspense' },
+    { nome: 'Janela Indiscreta', categoria: 'Suspense' },
+    { nome: 'O Sexto Sentido', categoria: 'Suspense' },
   ];
+  
 
   categoriaSelecionada = '';
   filmesFiltrados = this.filmes;
